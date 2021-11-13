a60dca5c43a666f4e1490cdba1747631dfa96454
Amanda Huang <79762895+AmandaJ-Huang@users.noreply.github.com>
2021-11-13 14:56:29 -0500

    Added Tests for sorting and created sort method, boolean currentLessThanPrevious

--- src/main/java/MainApplication.java
+++ src/main/java/MainApplication.java
@@ -1,5 +1,58 @@
+import java.util.Arrays;
+
 public class MainApplication {
     public static void main(String[] args) {
 
+    }
+
+    public String[] sortArray(String[] input) {
+        /*
+        Check each element
+        Compare one to the next via ASCII table
+        Set smallest variable to the first element,
+            compare to 2nd and swap if 2nd is smaller (bubble sort)
+         */
+        String smallestValue = input[0];
+        /*wrap the for loop in a while loop
+        while boolean !sorted
+         */
+        Boolean sorted = false;
+
+        while(!sorted) {
+            sorted = true;
+            for (int i = 1; i < input.length; i++) {
+                //if the 2nd num [i] is smaller than the 1st num [i-1]
+//                if ((input[i].length() == input[i - 1].length()
+//                        && input[i].compareTo(input[i - 1]) < 0)
+//                    || input[i].length() < input[i - 1].length()) {
+                if (currentIsLessThanPrevious(input[i], input[i-1])) {
+                    String temp = input[i];
+                    input[i] = input[i - 1];
+                    input[i - 1] = temp;
+                    sorted = false;
+                }
+//                if (input[i].length() < input[i - 1].length()) {
+//                    String temp = input[i];
+//                    input[i] = input[i - 1];
+//                    input[i - 1] = temp;
+//                    sorted = false;
+//                }
+            }
+        }
+        return input;
+    }
+
+    /*
+    Sub-method to see if current is less than previous
+     */
+    public Boolean currentIsLessThanPrevious(String current, String previous) {
+        if (current.length() < previous.length()) {
+            return true;
+        } else if (current.length() > previous.length()) {
+            return false;
+        } else if (current.compareTo(previous) < 0) {
+            return true;
+        }
+        return false;
     }
 }
--- src/test/java/TestMainApplication.java
+++ src/test/java/TestMainApplication.java
@@ -1,8 +1,85 @@
+import org.junit.Before;
 import org.junit.Test;
+import static org.junit.Assert.*;
 
 public class TestMainApplication {
-    @Test
-    public void test() {
+    /* Given: an array of numbers as strings, i.e., {"1", "5", "15", "9", "4"}
+    Task: sort the numbers
+    Restriction: the numbers must stay strings - no converting to integers
 
+    Result -- return an array of string numbers in the proper order: lowest --> highest 1, 4, 5, 9, 15
+
+    Expected: {"1", "4", "5", "9", "15"}
+
+     */
+    private MainApplication main = new MainApplication();
+
+
+    @Test
+    public void testWithPositives() {
+        //Given
+        String[] given = {"1", "5", "15", "9", "4"};
+        String[] expected = {"1", "4", "5", "9", "15"};
+
+        //When
+        String[] actual = main.sortArray(given);
+
+        //Then
+        assertArrayEquals(expected, actual);
+    }
+
+//    @Test
+//    public void testWithNegatives() {
+//        //Given
+//        String[] given = {"-2", "1", "5", "15", "9", "4"};
+//        String[] expected = {"-2", "1", "4", "5", "9", "15"};
+//
+//        //When
+//        String[] actual = main.sortArray(given);
+//
+//        //Then
+//        assertArrayEquals(expected, actual);
+//    }
+
+    /*
+    Sub-method test current less than previous
+     */
+    @Test
+    public void currentIsLessThanPrevious_True() {
+        //Given
+        String[] given = {"5", "1"};
+        Boolean expected = true;
+
+        //When
+        Boolean actual = main.currentIsLessThanPrevious(given[1], given[0]);
+
+        //Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void currentIsLessThanPrevious_False() {
+        //Given
+        String[] given = {"1", "5"};
+        Boolean expected = false;
+
+        //When
+        Boolean actual = main.currentIsLessThanPrevious(given[1], given[0]);
+
+        //Then
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    public void currentIsLessThanPrevious_WhenEqual() {
+        //Given
+        String[] given = {"1", "1"};
+        Boolean expected = false;
+
+        //When
+        Boolean actual = main.currentIsLessThanPrevious(given[1], given[1]);
+
+        //Then
+        assertEquals(expected, actual);
     }
 }
